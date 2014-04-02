ea5e2f9d1e7c2bfef982135c14f5ba29cde58f3c
Ramsey Nasser <ram@nas.sr>
2014-04-02 12:25:28 -0400

    Add jqconsole, bump to 0.2.0

--- js/plt.js
+++ js/plt.js
@@ -1,8 +1,8 @@
 /*
- * plt.js 0.1.0
- * 
+ * plt.js 0.2.0
+ *
  * http://github.com/nasser/pltjs
- * 
+ *
  * Copyright (c) 2014 Ramsey Nasser
  * Licensend under the MIT license.
  */
@@ -10,23 +10,49 @@
 // configuration object
 // refreshes every second by default
 var PLT = {
-  refresh: true,
+  refresh: false,
   refreshTime: 1000,
 
   parser: null
 };
 
-window.onload = function() {
+$(function() {
   // inject css style to format code elements and body text
   var cssNode = document.createElement('style');
   cssNode.innerHTML = "body { font-family: sans-serif; }";
   cssNode.innerHTML += "code { display: block; white-space: pre; margin-bottom: 1em; }";
+  cssNode.innerHTML += "#repl { height: 50px; }";
+  cssNode.innerHTML += "textarea { opacity:0 }";
+  cssNode.innerHTML += ".jqconsole-cursor { background: gray; }";
   document.body.appendChild(cssNode);
 
   // extract PEG grammar from <grammar> element and build parser
-  var grammarElement = document.querySelector("grammar");
-  PLT.parser = PEG.buildParser(grammarElement.textContent)
-  grammarElement.parentNode.removeChild(grammarElement);
+  var grammarElement = $("grammar");
+  PLT.parser = PEG.buildParser(grammarElement.text())
+  grammarElement.remove();
+
+  // build repl object
+  $('<div id="repl">').
+    appendTo("body");
+  var repl = $("#repl").jqconsole("", '> ');
+  $("#repl div").attr('style', '');
+
+  var startPrompt = function () {
+    repl.Prompt(true, function (input) {
+      try {
+        var evalfn = PLT.repl || PLT.eval || PLT.parser.parse;
+        repl.Write(PLT.parser.parse(input) + '\n', 'jqconsole-output');
+      } catch(err) {
+        repl.Append($("<span>" + err.message + "</span>").css('color', 'red'))
+        repl.Write("\n");
+      }
+      startPrompt();
+    });
+  }
+  startPrompt();
+
+  $('<h2>' + $('title').text() + '</h2>').
+    prependTo("body");
 
   // collect all correct code examples and try and parse them
   var goods = document.querySelectorAll("code:not([bad])")
@@ -61,6 +87,5 @@ window.onload = function() {
   }
 
   // refresh the page if refreshing is enabled
-  if(PLT.refresh)
-    setTimeout(function() { window.location.reload(true) }, PLT.refreshTime);
-}
+  setInterval(function() { if(PLT.refresh) window.location.reload(true) }, PLT.refreshTime);
+});
